a9d1652e3f1eefbc82fad99fecf76fd7dd6fab45
VUTNUR <vutnurnethajigoud@gmail.com>
2023-07-18 15:55:17 +0530

    assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,21 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let[name,setName]=useState("");
+  function addText(e){
+    setName(e.target.value);
+  }
   return (
     <div>
+      <div>Enter your name:</div>
+      <input type="text" onChange={addText}/>
+      {
+         name.length>0 && <p className="pTag">Hello {name}!</p>
+      }
+      
+
         {/* Do not remove the main div */}
     </div>
   )
